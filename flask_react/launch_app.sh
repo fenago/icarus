86cd5c48f4fa48bd20491bf9bcfa82c8c9b1191c
Dr. Ernesto Lee <socrates73@gmail.com>
2023-06-19 16:26:19 -0700

    Update launch_app.sh

--- flask_react/launch_app.sh
+++ flask_react/launch_app.sh
@@ -11,4 +11,5 @@ sleep 60
 python ./flask_demo.py &
 
 # assumes you've ran npm install already (dockerfile does this during build)
-cd react_frontend && npm run build && serve -s build
+# cd react_frontend && npm run build && serve -s build
+cd react_frontend && npm install && npm run build && npm install -g serve && serve -s build
